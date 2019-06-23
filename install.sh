9de07de3c9af8f8cde5cf22cb456600aae809d1f
rotemil <51912166+rotemil@users.noreply.github.com>
2019-06-23 09:42:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ export HOME=/root
 git clone https://github.com/mrvautin/adminMongo.git && cd adminMongo
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
-npm install
+npm install -g npm --unsafe-perm=true --allow-root
 cd
 # changing localhost to everyone.
 sudo sed -i 's/localhost/0.0.0.0/g' /home/ec2-user/adminmongo/app.js
